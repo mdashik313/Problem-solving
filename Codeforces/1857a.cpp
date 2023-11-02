ef8c4045afe54a99591645192d00edd3e92f28c6
MD Ashik <mdashik5360@gmail.com>
2023-11-02 20:23:33 +0600

    solvd

---
+++ Codeforces/1857a.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*prototypes*/
+void solve();
+
+#define mod							1000000007;
+#define ll							long long
+#define pi							acos(-1)
+#define setzero(a)				    memset(a,0,sizeof(a))
+#define pb(a)						push_back(a)
+#define fastIO ios_base::sync_with_stdio(true); cin.tie(NULL);
+#define fileIO  freopen("input.txt", "r", stdin); \
+                freopen("output.txt", "w", stdout);
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define nl cout<<"\n"
+#define exit return 0
+
+#define cinn(a) for(auto& i : a) cin>>i
+#define printa(a) for(auto& i : a) cout<<i<<" ";nl
+
+ll max(ll a,ll b){return a<b?b:a;}
+ll min(ll a,ll b){return a>b?b:a;}
+
+
+int main()
+{
+    fastIO
+    #ifndef ONLINE_JUDGE
+        fileIO
+    #endif
+    
+    int t;
+    
+    scanf("%d", &t);
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+void solve(){
+    int n;cin>>n;
+    int even=0,odd=0;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        if(x%2==0) even++;
+        else odd++;
+    }
+    if(odd==0) yes;
+    else if(odd%2!=0) no;
+    else yes;
+    
+    return;
+}
+
+
+
+
